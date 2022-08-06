de591340ef8927795512e5d1798608b14fd3ed1b
Yash Shah <yashromilshah@gmail.com>
2022-08-06 23:03:14 +0530

    backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app =express();
+
+app.get('/',(req,res)=>{
+    res.json({
+        mesg:'hello yash'
+    })
+})
+
+app.get('/yash',(req,res)=>{
+    res.send("hello")
+})
+
+
+app.listen(3000)
